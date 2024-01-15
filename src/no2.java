03ea52eec1bbe6995e67bf8f84dd1ae63a39ef30
Davi Pradipta <davipradipta456@gmail.com>
2024-01-15 12:37:24 +0700

    UASno2

--- src/no2.java
+++ src/no2.java
@@ -1,3 +1,4 @@
+import org.w3c.dom.css.Counter;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,25 +7,31 @@ import java.util.ArrayList;
 public class no2 {
     public static void main(String[] args) throws IOException {
         ArrayList<Integer> g = new ArrayList<>();
-        BufferedReader reader = new BufferedReader((new InputStreamReader((System.in))));
-        System.out.print("Input jumlah data yang akan di proses : ");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Input jumlah data yang akan di proses : ");
         int u = Integer.parseInt(reader.readLine());
         int counter=0;
-        for(int index = 0; index<u;index++) {
+        for (int index=0;index<u;index++){
             counter++;
-            System.out.print("Masukan Nama " + counter + " : ");
+            System.out.print("Masukan Nama " + counter+" : ");
             String name = reader.readLine();
-            System.out.print("Huruf Yang Akan Dihitung : ");
-            char s = (char) reader.read();
-            g = CountWords(name,s);
+            System.out.println("Huruf yang akan dihitung : ");
+            char s = reader.readLine().charAt(0); // Membaca karakter pertama dari input
+            int count = countWord(name, s);
+            g.add(count);
         }
-        for(int o: g ) {
-            System.out.print(g + " ");
+        for (int value : g) {
+            System.out.println(value);
         }
     }
+    private static int countWord(String str, char c){
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == c) {
+                count++;
+            }
+        }
+        return count;
 
-    private static ArrayList<Integer> CountWords(String name, char s) {
-        ArrayList<Integer> g = new ArrayList<>();
-        return g;
     }
 }
