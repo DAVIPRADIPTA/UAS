28991dc8c2a5469cb2f5c9dbcbe7148cf342a774
Davi Pradipta <davipradipta456@gmail.com>
2024-01-15 11:50:31 +0700

    UAS

--- src/uas.java
+++ src/uas.java
@@ -11,23 +11,23 @@ public class uas {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            System.out.print("Silahkan masukan kapasitas mesin kendaraan anda : ");
+            System.out.print("Silahkan masukan kapasitas mesin kendaraan anda (dalam cc): ");
             int kapasitasMesin = scanner.nextInt();
             scanner.nextLine();
 
-            String kendaraan = "Kendaraan roda 4 (cc: " + kapasitasMesin + ")";
+            String kendaraan = "Kapasitas mesin kendaraan " + kapasitasMesin + " cc";
 
             if (kapasitasMesin >= 1000 && kapasitasMesin <= 1200) {
                 lantai3.add(kendaraan);
-                System.out.println("Ditempatkan di lantai 3");
+                System.out.println("Kendaraan akan ditempatkan di lantai 3");
             } else if (kapasitasMesin >= 1500 && kapasitasMesin <= 2000) {
                 lantai2.add(kendaraan);
-                System.out.println("Ditempatkan di lantai 2");
+                System.out.println("Kendaraan akan ditempatkan di lantai 2");
             } else if (kapasitasMesin >= 2500) {
                 lantai1.add(kendaraan);
-                System.out.println("Ditempatkan di lantai 1");
+                System.out.println("Kendaraan akan ditempatkan di lantai 1");
             } else {
-                System.out.println("Kendaraan tidak memenuhi ketentuan.");
+                System.out.println("Kendaraan anda tidak memenuhi ketentuan.");
             }
 
             System.out.print("Apakah ada kendaraan lain? (ya/tidak): ");
@@ -38,18 +38,18 @@ public class uas {
             }
         }
 
-        // Menampilkan data kendaraan di setiap lantai
-        System.out.println("\nData Kendaraan di Lantai 1:");
+
+        System.out.println("Data Kendaraan di Lantai 1:");
         for (String kendaraan : lantai1) {
             System.out.println(kendaraan);
         }
 
-        System.out.println("\nData Kendaraan di Lantai 2:");
+        System.out.println("Data Kendaraan di Lantai 2:");
         for (String kendaraan : lantai2) {
             System.out.println(kendaraan);
         }
 
-        System.out.println("\nData Kendaraan di Lantai 3:");
+        System.out.println("Data Kendaraan di Lantai 3:");
         for (String kendaraan : lantai3) {
             System.out.println(kendaraan);
         }
